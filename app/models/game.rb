dd2383ba232880fee86002c5cfdad4657dbbb087
David Blezinger <david.w.blezinger@gmail.com>
2023-08-29 16:58:00 +0200

    made games new, create, show work

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ApplicationRecord
+  has_many :game_topics
+  has_many :game_participations
+  has_many :questions
   has_many :topics, through: :game_topics
   has_many :users, through: :game_participations
 end
