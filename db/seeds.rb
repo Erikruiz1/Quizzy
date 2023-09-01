a75397ae7026cfab80816757c0d876c5a01fd9af
David Blezinger <david.w.blezinger@gmail.com>
2023-09-01 13:54:30 +0200

    added destroy_all and topics to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+GameParticipation.destroy_all
+GameTopic.destroy_all
+Guess.destroy_all
+Hint.destroy_all
+Question.destroy_all
+User.destroy_all
+Game.destroy_all
+Topic.destroy_all
 
 # Seed Users
 users_data = [
@@ -20,16 +27,22 @@ users_data.each do |user_data|
 end
 
 topics_data = [
-  { name: "Chemistry", category: "Science" },
+  { name: "Chemical Elements and Symbols", category: "Chemistry" },
   { name: "Literary Classics", category: "Literature" },
+  { name: "Space Exploration Milestones", category: "Astronomy" },
   { name: "World Capitals", category: "Geography" },
   { name: "Famous Artists", category: "Art and Culture" },
+  { name: "Human Anatomy and Systems", category: "Science" },
   { name: "Biology Basics", category: "Biology" },
   { name: "Ancient Civilizations", category: "History" },
+  { name: "Historical Events of the 20th Century", category: "History" },
   { name: "Mathematical Puzzles", category: "Mathematics" },
+  { name: "Advanced Mathematical Concepts", category: "Mathematics" },
   { name: "Classical Music Composers", category: "Music" },
   { name: "Movie Quotes", category: "Film and Television" },
-  { name: "Famous Inventors", category: "Technology" },
+  { name: "World Religions and Belief Systems", category: "Religion and Philosophy" },
+  { name: "Famous Inventors", category: "Science and Technology" },
+  { name: "Modern Technology and Innovations", category: "Science and Technology" },
   { name: "Literary Quotes", category: "Literature" },
   { name: "mythological characters", category: "Mythology" },
   { name: "Famous Paintings", category: "Art and Culture" }
