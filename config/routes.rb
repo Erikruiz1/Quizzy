a5ced66837d79f8e9c985ba72b65af078bccbaa7
Paul Georgescu <paulgeorgescu94@icloud.com>
2023-09-04 15:13:02 +0200

    save

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  resources :users, only: [:show]
   resources :guesses, only: [:create]
   resources :topics, only: [:create]
 
