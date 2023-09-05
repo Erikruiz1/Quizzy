d851a64cb42222826c50c9d2824645862ae90309
Paul Georgescu <paulgeorgescu94@icloud.com>
2023-09-05 10:08:53 +0200

    added progress bar to completed and open games in the user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,37 @@
 class UsersController < ApplicationController
 
   def show
+    @display_states_completed = []
+    @display_states_open = []
+
     @games = current_user.games
-    
+    @completed_games = @games.order(:id).select do |game|
+      game.completed == true
+    end
+    @open_games = @games.order(:id).select do |game|
+      game.completed != true
+    end
+
+    @completed_games.each do |g|
+      @display_states_completed.push(progress_bar(g))
+    end
+    @open_games.each do |g|
+      @display_states_open.push(progress_bar(g))
+    end
+  end
+
+  private
+
+  def progress_bar(game)
+    display_states = []
+    game.questions.order(:id).each do |q|
+      unless q.guesses.empty?
+        q.guesses.last.correct == true ? display_states.push("correct") : display_states.push("incorrect")
+      else
+        display_states.push("empty")
+      end
+    end
+    display_states
   end
 
 end
